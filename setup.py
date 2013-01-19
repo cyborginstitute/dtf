31bf03882462450de5e9d6a25273721f65d6a19b
tycho garen <garen@tychoish.com>
2013-01-19 09:41:19 -0500

    infra: dependency info to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+REQUIRES = ['pyyaml']
+
+if version_info < (2, 7):
+    # no argparse in 2.6 standard
+    REQUIRES.append('argparse')
+
 setup(
     name='dtf',
     description='Documentation Testing Framework',
@@ -7,6 +13,7 @@ setup(
     license='Apache',
     url='http://cyborginstitute.org/projects/dtf',
     packages=['dtf'],
+    install_requires=REQUIRES
     test_suite=None,
     entry_points={
         'console_scripts': [
