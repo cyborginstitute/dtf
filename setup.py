d9a2283a8a34fd2168430de5b5dfa6f63330ab3f
tycho garen <garen@tychoish.com>
2013-01-22 14:22:32 -0500

    setup: updates to setup.py file, and BUMP for 0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
+from sys import version_info
 
-REQUIRES = ['pyyaml']
+REQUIRES = ['pyyaml', 'threadpool']
 
 if version_info < (2, 7):
     # no argparse in 2.6 standard
@@ -9,12 +10,21 @@ if version_info < (2, 7):
 setup(
     name='dtf',
     description='Documentation Testing Framework',
-    version='0.1.dev',
+    version='0.1',
     license='Apache',
     url='http://cyborginstitute.org/projects/dtf',
     packages=['dtf'],
-    install_requires=REQUIRES
+    install_requires=REQUIRES,
     test_suite=None,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: Apache Software License', 
+    ],
     entry_points={
         'console_scripts': [
             'dtf = dtf.dtf:main',
