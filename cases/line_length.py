c740f3f26c5932721a3bd89e19e8e76e54ee8f36
tycho garen <garen@tychoish.com>
2013-02-07 21:49:37 -0500

    test: adding single file line length testing and cleaned up tree line length

--- cases/line_length.py
+++ cases/line_length.py
@@ -12,52 +12,34 @@ class DtfLineLength(DtfCase):
         else: 
             return False
 
-    def render_source_tree(self): 
-        self.msg('crawling "%s" for files.' % self.case['directory'])
-
-        output = [ item for item 
-                        in expand_tree(self.case['directory'], self.case['extension'])
-                        if item not in self.case['exceptions']
-                ]
-
-        return output
-
-    def check_directory(self, p=None):
-        for source_file in self.render_source_tree():
-            ln = 1
-            with open(source_file, 'r') as f: 
-                ln += 1
-                for line in f.readlines():
-                    ln = ln + 1
-                    if self.check_line(line, self.case['max_length']):
-                        p = False
-                        failing = source_file
-                        break
-                
-            if p is False: 
-                break
-            else:
-                self.msg('checked line lengths in %s, which passed.' % source_file)
+    def check_file(self, source_file, p=True):
+        ln = 1
+        with open(source_file, 'r') as f:
+            ln += 1
+            for line in f.readlines():
+                ln = ln + 1
+                if self.check_line(line, self.case['max_length']):
+                    p = False
+                    break
 
         if p is False: 
-            return p, failing, ln
+            return p, ln
         else:
-            return True, p, p
-
+            return p, None
 
     def test(self):
-        result = self.check_directory()
+        result = self.check_file(self.case['file'])
 
         if result[0] is True: 
-            msg = ('[%s]: all files in %s have no lines longer than %s characters.' 
-                   % (self.name, self.case['directory'], self.case['max_length']))
+            msg = ('[%s]: %s has no lines longer than %s characters.' 
+                   % (self.name, self.case['file'], self.case['max_length']))
         else:
             msg = ('[%s]: line %s in "%s" is longer than %s characters.' 
-                   % (self.name, result[2], result[1], self.case['max_length']))
+                   % (self.name, result[1], self.case['file'], self.case['max_length']))
             
         return result[0], msg
 
 def main(name, case):
     c = DtfLineLength(name, case)
-    c.required_keys(['type', 'directory', 'extension', 'max_length', 'exceptions'])
+    c.required_keys(['file', 'max_length', 'name'])
     c.run()
---
+++ cases/tree_line_length.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+from cases import DtfCase
+from utils import expand_tree
+from line_length import DtfLineLength
+import dtf
+
+class DtfTreeLineLength(DtfLineLength):
+    def render_source_tree(self): 
+        self.msg('crawling "%s" for files.' % self.case['directory'])
+
+        output = [ item for item 
+                        in expand_tree(self.case['directory'], self.case['extension'])
+                        if item not in self.case['exceptions']
+                ]
+
+        return output
+
+    def check_directory(self, p=None):
+        for source_file in self.render_source_tree():
+            result = self.check_file(source_file)
+
+            if result[0] is False: 
+                failing = source_file
+                break
+            else:
+                self.msg('checked line lengths in %s, which passed.' % source_file)
+                continue
+
+        if p is False: 
+            return False, failing, result[1]
+        else:
+            return True, None, None
+
+
+    def test(self):
+        result = self.check_directory()
+
+        if result[0] is True: 
+            msg = ('[%s]: all files in %s have no lines longer than %s characters.' 
+                   % (self.name, self.case['directory'], self.case['max_length']))
+        else:
+            msg = ('[%s]: line %s in "%s" is longer than %s characters.' 
+                   % (self.name, result[2], result[1], self.case['max_length']))
+            
+        return result[0], msg
+
+def main(name, case):
+    c = DtfTreeLineLength(name, case)
+    c.required_keys(['type', 'directory', 'extension', 'max_length', 'exceptions', 'name'])
+    c.run()
